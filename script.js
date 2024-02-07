8f9512769c972d4385610c1defad3045600506e9
~~SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2024-02-07 12:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function fibonacci(num) {
-// your code here
-	if (num==2) {
-		return 1;
-	}
-	else if(num==1){
-		return 0;
-	}
-	return fibonacci(num-1)+fibonacci(num-1);
+    if (num <= 0) {
+        return 0;
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 }
 
 module.exports = fibonacci;
