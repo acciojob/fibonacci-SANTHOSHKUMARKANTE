acb81fa69f885787cd7fd5a77430c9b70dc5d9b0
~~SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2024-02-07 12:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function fibonacci(num) {
-    if (num <= 0) {
-        return 0;
-    } else if (num === 1) {
+    if (num === 1) {
         return 0;
     } else if (num === 2) {
         return 1;
-    } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
-    }
+	}
+	num -= 2;
+	var num1 =0;
+	var num2 = 1;
+	var sum;
+	var i=0;
+	for(int i =0; i < num; i++){
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+	}
+	return num2;
+    // } else if (num === 2) {
+    //     return 1;
+    // } else {
+    //     return fibonacci(num - 1) + fibonacci(num - 2);
+    // }
 }
 
 module.exports = fibonacci;
